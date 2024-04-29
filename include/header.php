648e0f0733904a1a2223998d2a90d3cdfb82f36e
smorelli <saverio.morelli@outlook.it>
2024-04-29 21:59:09 +0200

    Updated icons

--- include/header.php
+++ include/header.php
@@ -16,7 +16,7 @@ if (!isset($url_opengraph) || $url_opengraph == "") {
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
       rel="stylesheet">
 <link rel="stylesheet" href="/css/style.css"/>
-<link rel="icon" href="/images/icon.png"/>
+<link rel="icon" href="/images/icon.svg"/>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="https://unpkg.com/twemoji@13.1.0/dist/twemoji.min.js"></script>
 <script src="/js/script.js"></script>
